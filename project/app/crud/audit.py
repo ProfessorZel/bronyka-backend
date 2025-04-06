3d2fedacd6fa611e274d1a0041f4a2b93af9dc7f
ProfessorZel <rabota1schola11@gmail.com>
2025-04-06 15:21:10 +0300

    Починка бага с удалением аудита

--- project/app/crud/audit.py
+++ project/app/crud/audit.py
@@ -4,7 +4,6 @@ from datetime import datetime, timedelta
 
 from sqlalchemy import delete
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.testing.assertsql import Or
 
 from app.crud.base import CRUDBase
 from app.models.audit import AuditEvent
@@ -18,12 +17,12 @@ class CRUDAuditEvent(CRUDBase):
     ):
          await session.execute(
             delete(AuditEvent).where(
-                #  И время окончания бронирования больше текущего времени
-                Or(
                     AuditEvent.time <= datetime.now() - timedelta(days=days),
-                    AuditEvent.time == None
-               )
-
+            )
+         )
+         await session.execute(
+            delete(AuditEvent).where(
+                AuditEvent.time == None
             )
          )
          await session.commit()
